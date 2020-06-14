d6123aa716c60d6719852f4b054983bc397df8cf
sarath-centative <sarath@centative.com>
2020-06-14 12:55:13 +0530

    Updated the date

--- js/main.js
+++ js/main.js
@@ -50,7 +50,7 @@ $(window).load(function() {
 /* END ------------------------------------------------------- */
 
 $("#countdown").countdown({
-  date: "Jun 01, 2020 12:01:00",
+  date: "Jun 21, 2020 12:01:00",
   render: function(data) {
     var el = $(this.el);
     el.empty()
